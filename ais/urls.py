4917e5643248ac5d75b83d778485e195f31def6b
ELY909 <marnkohaku@gmail.com>
2024-10-01 12:15:27 +0800

    Review 4

--- ais/urls.py
+++ ais/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('about/', views.about, name='about'),
     path('', views.homepage, name='homepage'),
+    path('student/', views.student_index, name='student_index'),
+    path('student/create/', views.student_create, name='student_create'),
 ]
+
--- ais/views.py
+++ ais/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import StudentsForm
+from .models import Students
 
-# Create your views here.
 def homepage(request):
     return render(request, 'homepage/index.html')
 
 def about(request):
     return render(request, 'homepage/about.html')
+
+
+def student_index(request):
+    students = Students.objects.all()
+    return render(request, 'student/index.html', {'students': students})
+
+def student_create(request):
+    if request.method == 'POST':
+        form = StudentsForm(request.POST)
+        if form.is_valid():
+            form.save()  
+            messages.success(request, 'Mahasiswa berhasil dibuat!')  
+            return redirect('student_index') 
+    else:
+        form = StudentsForm()
+    return render(request, 'student/create.html', {'form': form})
